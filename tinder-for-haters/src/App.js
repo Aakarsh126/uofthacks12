55f4db9d1cbae4df2de4c23a74caf70f8103edf9
Aakarsh Garg <116822559+Aakarsh126@users.noreply.github.com>
2025-01-19 04:55:18 -0500

    added routing

--- tinder-for-haters/src/App.js
+++ tinder-for-haters/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Home from './pages/Home';
 import Likes from './pages/Likes';
 import Messages from './pages/Messages';
@@ -13,6 +15,8 @@ function App() {
       <div className="App">
         <div className="content">
           <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/" element={<Home />} />
             <Route path="/likes" element={<Likes />} />
             <Route path="/messages" element={<Messages />} />
